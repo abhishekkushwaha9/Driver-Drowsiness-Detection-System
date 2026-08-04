c7f4f8985a095c7ec237391d87c8c348987e2723
Abhishek Kushwaha <abhishekkushwaha7928@gmail.com>
2026-08-04 18:57:26 +0530

    Disable debug mode in app.py

--- backend/app.py
+++ backend/app.py
@@ -70,4 +70,4 @@ def status():
 # -----------------------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=True)
+    socketio.run(app, host="0.0.0.0", port=port, debug=False)
